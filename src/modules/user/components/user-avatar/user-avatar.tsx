cad0cd96d9035042b9248b8c6960461d80b3a8dc
Fer Coopat <fercoopat950211@gmail.com>
2025-08-16 14:27:40 -0400

    fix: avatar image added to public folder.

--- src/modules/user/components/user-avatar/user-avatar.tsx
+++ src/modules/user/components/user-avatar/user-avatar.tsx
@@ -1,16 +1,13 @@
-import { LoaderCircleIcon } from 'lucide-react';
+import { LoaderCircleIcon } from "lucide-react";
 
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const UserAvatar = () => {
   return (
-    <Avatar className='size-[43px] md:ml-4'>
-      <AvatarImage
-        src='https://github.com/shadcn.png'
-        alt='avatar placeholder'
-      />
-      <AvatarFallback>
-        <LoaderCircleIcon className='animate-spin mx-auto size-[32px] md:size-[20px]' />
+    <Avatar className="size-[43px] md:ml-4">
+      <AvatarImage src="/images/avatar.webp" alt="avatar placeholder" />
+      <AvatarFallback className="p-1">
+        <LoaderCircleIcon className="size-full animate-spin" />
       </AvatarFallback>
     </Avatar>
   );
